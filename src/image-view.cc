024269c8ceb1c03029081ac31b14f9ea7b38af0d
Romain Goffe <romain.goffe@gmail.com>
2013-07-13 22:19:04 +0200

    resize the image view after building the scene

--- src/image-view.cc
+++ src/image-view.cc
@@ -138,6 +138,8 @@ void CImageView::setModel(CMatrixModel * model)
   m_scene->setSceneRect(QRect(0, 0, m_image->width(), m_image->height()));
   m_scene->addPixmap(QPixmap::fromImage(*m_image));
   m_scene->addItem(m_selectionBox);
+
+  fitToWindow();
 }
 
 CMainWindow* CImageView::parent() const
